45cfefe850c6e7eecef61522b6e1a61f8b46069a
nuta88 <anna.vagner-88@mail.ru>
2017-07-14 18:44:36 +0300

    Change the appearance of site and mark out of word.

--- event.js
+++ event.js
@@ -2,12 +2,13 @@ window.onload = function() {
 	document.getElementById("button").onclick = function() {
 		var text = getText();
 		deleteDivInput();
-		var divText = document.getElementById("divText");
-		deleteInputedTextElement(divText);
-		divText.innerHTML += convertTextToSpans(text);
-		highlightWordsEvent(divText.getElementsByTagName("span"));
-		clickForMovingWordFromTextToDiction(divText
-				.getElementsByTagName("span"));
+		var createDivText = createDiv("divText", "pText", "text");
+		var createDivDiction = createDiv("divDiction", "pDiction", "dictionary");
+		var button = creatButton();
+		var pText = document.getElementById("pText");
+		pText.innerHTML += convertTextToSpans(text);
+		highlightWordsEvent(pText.getElementsByTagName("span"));
+		clickForMovingWordFromTextToDiction(pText.getElementsByTagName("span"));
 	};
 };
 
@@ -20,18 +21,35 @@ function deleteDivInput() {
 	document.getElementById("body").removeChild(child);
 };
 
-function convertTextToSpans(text) {
-	return "<span class='empty'>"
-			+ text.replace(/\s+/ig, "</span> <span class='empty'>") + "</span>";
-};
+function createDiv(attributeDiv, attributeParag, value) {
+	var div = document.createElement("div");
+	div.setAttribute("id", attributeDiv);
+	div.setAttribute("class", attributeDiv);
+	body.appendChild(div);
+	var h1 = document.createElement("h1");
+	h1.innerHTML = value;
+	div.appendChild(h1);
+	var p = document.createElement("p");
+	p.setAttribute("id", attributeParag);
+	div.appendChild(p);
+}
 
-function deleteInputedTextElement(parentElement) {
-	var child = parentElement.getElementsByTagName("p")[0];
-	parentElement.removeChild(child);
+function creatButton() {
+	var button = document.createElement("button");
+	button.setAttribute("id", "buttonReload");
+	button.innerHTML = "Input the text";
+	button.onclick = function() {
+		window.location.reload();
+	};
+	body.appendChild(button);
+}
+
+function convertTextToSpans(text) {
+	return "<span class='empty'>" + text.replace(/\s+/ig, "</span> <span class='empty'>") + "</span>";
 };
 
 function highlightWordsEvent(spans) {
-	for ( var i = 0; i < spans.length; i++) {
+	for (var i = 0; i < spans.length; i++) {
 		spans[i].onmouseover = showAllocate;
 		spans[i].onmouseout = showEmpty;
 	}
@@ -45,12 +63,12 @@ function showEmpty(event) {
 	event.target.setAttribute("class", "empty");
 };
 
-function showAllocateRed(spanText) {
-	spanText.setAttribute("class", "allocateRed");
+function showAllocateWord(spanText) {
+	spanText.setAttribute("class", "allocate");
 };
 
 function clickForMovingWordFromTextToDiction(words) {
-	for ( var i = 0; i < words.length; i++) {
+	for (var i = 0; i < words.length; i++) {
 		words[i].onclick = moveWordFromTextToDiction;
 	}
 };
@@ -70,30 +88,51 @@ function setEventsOfLastDictionSpan(lastSpan) {
 	lastSpan.onmouseover = showAllocate;
 	lastSpan.onmouseout = showEmpty;
 	lastSpan.onclick = allocateWordsOnText;
-
 };
 
 function allocateWordsOnText(event) {
-	for ( var i = 0; i < register.allocatedSpans.length; i++) {
+	for (var i = 0; i < register.allocatedSpans.length; i++) {
 		register.allocatedSpans[i].setAttribute("class", "empty");
 	}
 	register.clearRegister();
+	allocateWordsOnDiction(event.target);
 
-	var divText = document.getElementById("divText");
-	var allSpansOfText = divText.getElementsByTagName("span");
+	var pText = document.getElementById("pText");
+	var allSpansOfText = pText.getElementsByTagName("span");
 	var wordToSearch = event.target.innerHTML;
-	for ( var i = 0; i < allSpansOfText.length; i++) {
+	for (var i = 0; i < allSpansOfText.length; i++) {
 		if (allSpansOfText[i].innerHTML.search(wordToSearch) != -1) {
 			var textConvert = convertSpanToTextWithoutSigns(allSpansOfText[i].innerHTML);
 			if (textConvert === wordToSearch) {
-				showAllocateRed(allSpansOfText[i]);
+				showAllocateWord(allSpansOfText[i]);
 				register.addToRegister(allSpansOfText[i]);
 			}
 		}
 	}
 };
 
+function allocateWordsOnDiction(event) {
+	for (var i = 0; i < registerDiction.allocatedSpans.length; i++) {
+		registerDiction.allocatedSpans[i].setAttribute("class", "empty");
+	}
+	registerDiction.clearRegister();
+	showAllocateWord(event);
+	event.onmouseout = showAllocateWord;
+	event.onmouseover = showAllocateWord;
+	registerDiction.addToRegister(event);
+}
+
 var register = {
+	allocatedSpans : [],
+	addToRegister : function(span) {
+		this.allocatedSpans.push(span);
+	},
+	clearRegister : function() {
+		this.allocatedSpans = [];
+	}
+};
+
+var registerDiction = {
 	allocatedSpans : [],
 	addToRegister : function(span) {
 		this.allocatedSpans.push(span);
